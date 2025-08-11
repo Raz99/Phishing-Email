d124b4d4c20e57c2c398a32d9829b789936e0b7a
Raz Cohen <54115787+Raz99@users.noreply.github.com>
2025-08-11 17:01:59 +0300

    Simplify the first lines in the code

--- attach_create.py
+++ attach_create.py
@@ -1,6 +1,4 @@
-# Aliza Lazar – 336392899
-# Raz Cohen – 208008995
-
+# Raz Cohen, Aliza Lazar
 import os # For system commands and environment variables
 import shutil # For file and directory operations
 from PyInstaller.__main__ import run # PyInstaller's run function to create the executable
